e4d7a8f9cc61ef29eae5dc4e25ed79503bdaddc1
jack pan <jackpanyj@gmail.com>
2025-03-19 17:58:41 +0800

    feat: save

--- app/playground/layout.tsx
+++ app/playground/layout.tsx
@@ -5,7 +5,9 @@ import Providers from "../providers";
 export default function Layout({ children }) {
   return (
     <Providers>
-      <div className={clsx("h-screen flex flex-col overflow-auto")}>
+      <div
+        className={clsx("h-screen flex flex-col overflow-auto bg-[#1b202c]")}
+      >
         <div className="grow max-w-view px-4 mx-auto w-full flex flex-col">
           {children}
         </div>

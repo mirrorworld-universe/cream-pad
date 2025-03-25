3cd43fdd6e3220401ccae049078a0fedd8e520c2
jack pan <jackpanyj@gmail.com>
2025-03-25 18:13:05 +0800

    feat: done

--- app/components/common/PrimaryButton.tsx
+++ app/components/common/PrimaryButton.tsx
@@ -4,7 +4,7 @@ export default function PrimaryButton({ className, ...props }: ButtonProps) {
   return (
     <Button
       className={cn(
-        "h-12 px-6 font-bold text-white bg-black rounded-full cursor-pointer",
+        "h-12 px-6 font-bold text-white bg-black rounded-full transition-all cursor-pointer hover:bg-[#292929] hover:shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]",
         className
       )}
       {...props}
